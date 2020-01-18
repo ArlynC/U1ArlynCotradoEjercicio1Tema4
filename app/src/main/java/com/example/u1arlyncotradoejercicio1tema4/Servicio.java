bce57209d97cfbc83df7e8c214a094af094f75b4
Arlyn Cotrado <alejandra321.any@gmail.com>
2020-01-18 15:47:52 -0500

    notificacion de llamada

--- app/src/main/java/com/example/u1arlyncotradoejercicio1tema4/Servicio.java
+++ app/src/main/java/com/example/u1arlyncotradoejercicio1tema4/Servicio.java
@@ -1,18 +1,102 @@
 package com.example.u1arlyncotradoejercicio1tema4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
-public class Servicio extends AppCompatActivity {
+public class Servicio extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //enciende el celular aunque este en pantalla de bloqueo
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
         setContentView(R.layout.activity_servicio);
-        startService(new Intent(Servicio.this,
-                ServicioEspia.class));
-        finish();
+
+
+
+
+        if ((ContextCompat.checkSelfPermission(Servicio.this,
+                Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED)){
+            arranque();
+        } else {
+            Handler handler = new Handler();
+            handler.postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            if (ContextCompat.checkSelfPermission(Servicio.this,
+                                    Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+                                permisolocalizacion();
+                            } else {
+                                solicitarPermiso(Manifest.permission.CALL_PHONE,
+                                        "Sin el permiso" + " de estado de telefono", 0);
+                            }
+                        }
+                    }, 2000L);
+        }
+    }
+
+
+    void permisolocalizacion(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED) {
+            arranque();
+        } else {
+            solicitarPermiso(Manifest.permission.READ_CALL_LOG,
+                    "Sin el permiso" + " de registro de llamadas", 1);
+        }
+    }
+
+
+    public void solicitarPermiso(final String permiso, String justificacion, final int codigo) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permiso)) {
+            AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+            dialogo1.setTitle("Solicitud de permiso");
+            dialogo1.setMessage(justificacion);
+            dialogo1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogo1, int id) {
+                    ActivityCompat.requestPermissions(Servicio.this, new String[]{permiso}, codigo);
+                }
+            });
+            dialogo1.show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{permiso}, codigo);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                permisolocalizacion();
+            } else {
+                solicitarPermiso(Manifest.permission.READ_PHONE_STATE,
+                        "Sin el permiso" + " de telefono no podemos realizar llamadas.", 0);
+            }
+        }
+        if (requestCode == 1) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                arranque();
+            } else {
+                solicitarPermiso(Manifest.permission.READ_CALL_LOG,
+                        "Sin el permiso" + " de ubicacion no podremos localizarte", 1);
+            }
+        }
+    }
+    void arranque() {
+        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
     }
 }
--- app/src/main/java/com/example/u1arlyncotradoejercicio1tema4/ServicioEspia.java
+++ app/src/main/java/com/example/u1arlyncotradoejercicio1tema4/ServicioEspia.java
@@ -1,19 +1,56 @@
 package com.example.u1arlyncotradoejercicio1tema4;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.SystemClock;
+import android.text.Html;
+
+import androidx.core.app.NotificationCompat;
+
+import static com.example.u1arlyncotradoejercicio1tema4.ReceptorLlamada.NOTIFICATION_CHANNEL_ID;
 
 public class ServicioEspia extends Service {
-    @Override
-    public int onStartCommand(Intent i, int flags, int idArranque) {
-       // SystemClock.sleep(25000);
 
-        return START_NOT_STICKY;
+
+    private static final int ID_NOTIFICACION_CREAR = 1;
+    public static final String NOTIFICATION_CHANNEL_ID = "1000";
+    public static final String NOTIFICATION_CHANNEL_NAME = "UNJBG";
+
+
+    @Override //notificacion
+    public int onStartCommand(Intent intenc, int flags, int idArranque) {
+        Intent intencionLlamar = new Intent(Intent.ACTION_DIAL,
+                Uri.parse("tel:555123456"));
+        PendingIntent intencionPendienteLlamar =
+                PendingIntent.getActivity(this,0, intencionLlamar,0);
+
+        NotificationCompat.Builder notific = new NotificationCompat.Builder(this);
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { //si es mayor a android oreo=8
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(
+                            NOTIFICATION_CHANNEL_ID,
+                            NOTIFICATION_CHANNEL_NAME,
+                            NotificationManager.IMPORTANCE_LOW);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(R.color.colorAccent);
+            notificationManager.createNotificationChannel(notificationChannel);
+            notific.setChannelId(NOTIFICATION_CHANNEL_ID);
+
+        }
+        return START_STICKY;
     }
-    @Override
-    public IBinder onBind(Intent arg0) {
+    @Override public IBinder onBind(Intent intencion) {
         return null;
     }
 }
